14335c7ab4d026027311a34164e44309b82de851
Thomas <moras.thomas@gmail.com>
2022-01-17 23:55:08 +0100

    Commit test 2 vs code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
 import ReactDOM from "react-dom";
-
-import Table from "./home/table";
-import SerieList from "./home/home";
-import TechView from "./home/test";
-import Game from "./tuto-morpion/Game";
-import NavBar from "./pages/routing";
 
 function tick() {
   const element = (
@@ -23,29 +15,9 @@ function tick() {
 function App() {
   return (
     <div>
-      {NavBar()}
-      <div>
-        <Game />
-      </div>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <div className="container"></div>
+        <header classNams="App-header">
 
-          {/* <Table />
-      <SerieList/>
-      <TechView/> */}
         </header>
       </div>
     </div>
